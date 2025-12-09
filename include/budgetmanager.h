f11ba002cddb5ce8e1fd98692e79ef74c2ba139b
JordanAyl <jordandayling@gmail.com>
2025-12-08 20:27:36 -0800

    Expense and Income add/subtractor in budgetmanager.cpp. Added file Class. Added category Class.

--- include/budgetmanager.h
+++ include/budgetmanager.h
@@ -1,11 +1,47 @@
 #ifndef BUDGETMANAGER_H
 #define BUDGETMANAGER_H
+#include <QString>
+#include <QDate>
+#include <QVector>
 
-class BudgetManager
-{
+struct Income {
+    QString name;
+    double amount;
+    QDate date;
+};
+
+struct Expense {
+    QString name;
+    double amount;
+    QDate date;
+    QString category;
+};
+
+
+class BudgetManager {
 public:
     BudgetManager();
-    void addExpense();
+
+    // add items
+    void addIncome(const Income &income);
+    void addExpense(const Expense &expense);
+
+    // remove items
+    void removeIncomeAt(int index);
+
+    // totals
+    double totalIncome() const;
+    double totalExpenses() const;
+    double netBalance() const;
+
+
+    // getters for data
+    const QVector<Income>& getIncomes() const;
+    const QVector<Expense>& getExpenses() const;
+
+private:
+    QVector<Income> member_incomes;
+    QVector<Expense> member_expenses;
 };
 
 #endif // BUDGETMANAGER_H
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -7,7 +7,7 @@
 #include <QtCharts/QChart>
 #include <QtCharts/QPieSlice>
 
-//includes are for the resize animation
+//includes are for the resize animations
 #include <QPainter>
 #include <QTabWidget>
 #include <QPropertyAnimation>
@@ -43,6 +43,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     ////////////////// Connections ///////////////////////////
 
+    //need to change everything from foo bar call to connect()
     //this connects signal of the tab index to onTabChanged to create a tab animation.
     connect(ui->tabWidget, &QTabWidget::currentChanged,
             this, &MainWindow::onTabChanged);
@@ -68,13 +69,24 @@ MainWindow::MainWindow(QWidget *parent)
 
 }
 
+//change from foo bar to connection
 //wages button adds text from QStrings and date from QDate to the wages table/
 void MainWindow::on_wagesAddButton_clicked()
 {
-
     QString text1 = ui->wagesLine1->text();
     QString text2 = ui->wagesLine2->text();
     QDate dateEditDate = ui->dateWages->date();
+
+    //convert amount string to double
+    double amount = text2.toDouble();
+
+
+    Income income;
+    income.name = text1;
+    income.amount = amount;
+    income.date = dateEditDate;
+
+    member_budget.addIncome(income); //member_budget instantiated in the .h
 
     int row = ui->wagesTable->rowCount();
     ui->wagesTable->insertRow(row);
@@ -83,18 +95,33 @@ void MainWindow::on_wagesAddButton_clicked()
     ui->wagesTable->setItem(row, 1, new QTableWidgetItem(text2));
     ui->wagesTable->setItem(row, 2, new QTableWidgetItem(dateEditDate.toString("yyyy-MM-dd")));
 
-    // clear inputs after adding
+
+    //Update the last-page wages summary table
+    appendIncomeToSummary(amount);
+
+    //clear inputs after adding
     ui->wagesLine1->clear();
     ui->wagesLine2->clear();
 }
 
+//change from foo bar to connection
 void MainWindow::on_expensesAddButton_clicked()
 {
-
-    QString text1 = ui->expenseName->text();
-    QString text2 = ui->expenseAmount->text();
+    QString text1   = ui->expenseName->text();        // expense name
+    QString text2   = ui->expenseAmount->text();      // amount as string
     QString category = ui->categoryExpense->currentText();
     QDate dateEditDate = ui->dateExpense->date();
+
+    //convert amount to double
+    double amount = text2.toDouble();
+
+    Expense expense;
+    expense.name     = text1;
+    expense.amount   = amount;
+    expense.category = category;
+    expense.date     = dateEditDate;
+
+    member_budget.addExpense(expense); //member_budget instantiated in the .h
 
     int row = ui->expenseTable->rowCount();
     ui->expenseTable->insertRow(row);
@@ -104,37 +131,39 @@ void MainWindow::on_expensesAddButton_clicked()
     ui->expenseTable->setItem(row, 2, new QTableWidgetItem(category));
     ui->expenseTable->setItem(row, 3, new QTableWidgetItem(dateEditDate.toString("yyyy-MM-dd")));
 
+
     //clear text from expense text line
     ui->expenseName->clear();
     ui->expenseAmount->clear();
-
 }
 
 //wage delete selected button code
 void MainWindow::on_deleteWageButton_clicked()
 {
-    // Which row is currently selected?
     int row = ui->wagesTable->currentRow();
-
-    // If nothing is selected, do nothing
     if (row < 0)
         return;
 
-    // Remove row
+    //remove row from vector
+    member_budget.removeIncomeAt(row); //member_budget instantiated in the .h
+
+    //remove row from wages table
     ui->wagesTable->removeRow(row);
+
+    //Rebuild the wages column in the final table
+    refreshSummaryWagesColumn();
 }
 
 //wage delete selected button code
 void MainWindow::on_deleteExpenseButton_clicked()
 {
-    // Which row is currently selected?
     int row = ui->expenseTable->currentRow();
 
-    // If nothing is selected, do nothing
+
     if (row < 0)
         return;
 
-    // Remove that row
+    // Remove row
     ui->expenseTable->removeRow(row);
 }
 
@@ -144,13 +173,13 @@ void MainWindow::onTabChanged(int index)
     QSize targetSize;
 
     switch (index) {
-        case 2: // Charts tab
+        case 2: // Pie tab
             targetSize = QSize(1200, 800);
             break;
-        case 1: // Budget tables tab
+        case 1: // Expense tab
             targetSize = QSize(730, 500);
             break;
-        default: // Home / other tabs
+        default: // Income tab
             targetSize = QSize(625, 500);
             break;
     }
@@ -161,6 +190,61 @@ void MainWindow::onTabChanged(int index)
     anim->setEndValue(targetSize);
     anim->setEasingCurve(QEasingCurve::InOutCubic);
     anim->start(QAbstractAnimation::DeleteWhenStopped);
+}
+
+void MainWindow::appendIncomeToSummary(double amount)
+{
+    QTableWidget *table = ui->tableBudget;
+
+    // Make sure it has at least 3 columns: Wages, Expenses, Total Left Over
+    if (table->columnCount() < 3) {
+        table->setColumnCount(3);
+        QStringList headers;
+        headers << "Wages" << "Expenses" << "Total Left Over";
+        table->setHorizontalHeaderLabels(headers);
+    }
+
+    // New row at the bottom
+    int row = table->rowCount();
+    table->insertRow(row);
+
+    // Put the income amount in the Wages column (col 0), same column every time
+    auto *wagesItem = new QTableWidgetItem(QString::number(amount, 'f', 2));
+    table->setItem(row, 0, wagesItem);
+
+}
+
+void MainWindow::refreshSummaryWagesColumn()
+{
+    QTableWidget *table = ui->tableBudget;
+    const auto &incomes = member_budget.getIncomes();
+
+    // Make sure we have at least as many rows as incomes
+    if (table->rowCount() < incomes.size()) {
+        table->setRowCount(incomes.size());
+    }
+
+    int rowCount = table->rowCount();
+
+    // For each row:
+    // - if there's an income at that index, show its amount in col 0
+    // - otherwise clear the wages cell (but leave other columns alone)
+    for (int row = 0; row < rowCount; ++row) {
+
+        QTableWidgetItem *item = table->item(row, 0);
+        if (!item) {
+            item = new QTableWidgetItem();
+            table->setItem(row, 0, item);
+        }
+
+        if (row < incomes.size()) {
+            // There *is* an income for this row index
+            item->setText(QString::number(incomes[row].amount, 'f', 2));
+        } else {
+            // No income for this row index anymore → clear wages only
+            item->setText("");
+        }
+    }
 }
 
 MainWindow::~MainWindow()
